4650410ecf5d8cc266fc13b5aceb7cbbe38866ed
igobyblue <noahhernandez960@gmail.com>
2023-08-05 15:39:09 -0700

    bhbhjbhbh

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(years) {
+ //let n = Number.isInteger(years)
+  //let wholeYear = years[n];
+  //let lastNumber = years[n-1];
+  //let secondLastNumber = years[n-2];
+ if ((years % 4 == 0 && years % 100 != 0) || (years % 400 == 0)) {
+    return true;
+ }
 
+ else {
+    return false;
+ }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let roundedF = fahrenheit;
+  let numberF = (roundedF - 32) * 5/9;
+
+  return parseFloat(numberF.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let roundedC = celsius;
+  let numberC = (roundedC * 9/5) + 32 
+  
+  return parseFloat(numberC.toFixed(1));
+  
+  
 };
+
 
 // Do not edit below this line
 module.exports = {
